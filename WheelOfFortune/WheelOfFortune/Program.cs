5db87431298d0550c46edbc818e362e3ef8189b0
Qianqian Hu <v-qianh@microsoft.com>
2019-05-06 12:40:08 -0700

    Initialize commit

---
+++ WheelOfFortune/WheelOfFortune/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WheelOfFortune
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
